3290bd80f7a9588f613513f65cb512497883df8d
Robert Lee <bobbydwise1@hotmail.com>
2019-08-18 14:13:05 -0700

    Initial commit.

---
+++ scripts.js
@@ -0,0 +1,23 @@
+/*
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Google.
+
+The power set of a set is the set of all its subsets. Write a function that, given a set, generates its power set.
+
+For example, given the set {1, 2, 3}, it should return {{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
+
+You may also use a list or array to represent a set.
+*/
+
+
+
+console.log(root)
+
+$(document).ready(function() {
+  $('#form1').submit(function(event) {
+  event.preventDefault();
+  let input1 = $('#input1').val();
+  $('#output-section-1').text(input1);
+  })
+});
